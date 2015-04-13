3753fa8d3d54476143be79d0645a3b07199ed57a
Fernando Barreiro Megino <fh.barreiro@gmail.com>
2015-04-13 15:15:26 +0200

    Fixed calls to renamed sysconfig file [OSG standardization]

--- pandaserver/test/add.py
+++ pandaserver/test/add.py
@@ -234,7 +234,7 @@ class ForkThr (threading.Thread):
 
     def run(self):
         setupStr  = 'source %s; ' % panda_config.glite_source
-        setupStr += 'source /etc/sysconfig/panda_server-sysconfig; '
+        setupStr += 'source /etc/sysconfig/panda_server; '
         runStr  = '%s/python -Wignore ' % panda_config.native_python
         runStr += panda_config.pandaPython_dir + '/dataservice/forkSetupper.py -i '
         runStr += self.fileName
