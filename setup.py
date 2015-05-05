be81a2a535efe54726b1a4601e6793e5c5a43fb2
Fernando Barreiro Megino <fh.barreiro@gmail.com>
2015-05-05 10:35:51 +0200

    Added test/lsst/ and test/alice/ to setup.py packages

--- setup.py
+++ setup.py
@@ -169,6 +169,8 @@ setup(
                'pandaserver.server',
                'pandaserver.taskbuffer',
                'pandaserver.test',
+               'pandaserver.test.lsst',
+               'pandaserver.test.alice',
                'pandaserver.userinterface',
                'pandaserver.proxycache',
               ],
