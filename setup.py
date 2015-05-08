46b579c969c6b10d15c667a4b1344a5bb7795bf6
Fernando Barreiro Megino <fh.barreiro@gmail.com>
2015-05-08 15:31:46 +0200

    Adding post-install section to remove manual steps. First action is to create log directories if they don't exist.

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ import sys
 sys.path.insert(0,'.')
 
 import os
+import pwd
+import grp
 import re
 import socket
 import commands
@@ -150,6 +152,13 @@ class install_data_panda (install_data_org):
         self.data_files = new_data_files
         install_data_org.run(self)
         
+        #post install
+        uid = pwd.getpwnam(panda_user).pw_uid
+        gid = grp.getgrnam(panda_group).gr_gid
+        for directory in ['/var/log/panda/wsgisocks', '/var/log/panda/fastsocks']:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+                os.chown(directory, uid, gid)                
         
 # setup for distutils
 setup(
