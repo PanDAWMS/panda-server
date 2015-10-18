20cf0da40c12d1834b18fbcc8982c5072e292211
tmaeno <tmaeno@bnl.gov>
2015-10-18 15:53:47 +0200

    diabled inputFileCheck in Adder for pilotRetry

--- pandaserver/dataservice/AdderGen.py
+++ pandaserver/dataservice/AdderGen.py
@@ -20,6 +20,7 @@ from pandalogger.PandaLogger import PandaLogger
 from pandalogger.LogWrapper import LogWrapper
 from taskbuffer import EventServiceUtils
 from taskbuffer import retryModule
+import taskbuffer.ErrorCode
 
 # logger
 _logger = PandaLogger().getLogger('Adder')
@@ -93,16 +94,17 @@ class AdderGen:
                 self.logger.error('wrong attemptNr -> job=%s <> %s' % (self.job.attemptNr,self.attemptNr))
             else:
                 # check file status in JEDI
-                fileCheckInJEDI = self.taskBuffer.checkInputFileStatusInJEDI(self.job)
-                self.logger.debug("check file status in JEDI : {0}".format(fileCheckInJEDI))                
-                if fileCheckInJEDI == None:
-                    raise RuntimeError,'failed to check file status in JEDI'
-                if fileCheckInJEDI == False:
-                    # set job status to failed since some file status is wrong in JEDI 
-                    self.jobStatus = 'failed'
-                    self.job.ddmErrorCode = ErrorCode.EC_Adder
-                    self.job.ddmErrorDiag = "wrong file status in JEDI"
-                    self.logger.debug("set jobStatus={0} since input are already cancelled in JEDI".format(self.jobStatus))
+                if not self.job.taskBufferErrorCode in [taskbuffer.ErrorCode.EC_PilotRetried]:
+                    fileCheckInJEDI = self.taskBuffer.checkInputFileStatusInJEDI(self.job)
+                    self.logger.debug("check file status in JEDI : {0}".format(fileCheckInJEDI))                
+                    if fileCheckInJEDI == None:
+                        raise RuntimeError,'failed to check file status in JEDI'
+                    if fileCheckInJEDI == False:
+                        # set job status to failed since some file status is wrong in JEDI 
+                        self.jobStatus = 'failed'
+                        self.job.ddmErrorCode = ErrorCode.EC_Adder
+                        self.job.ddmErrorDiag = "wrong file status in source database"
+                        self.logger.debug("set jobStatus={0} since input are already cancelled in JEDI".format(self.jobStatus))
                 # keep old status
                 oldJobStatus = self.job.jobStatus
                 # set job status
