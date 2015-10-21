7977760d5f025c13cad8c8315b62b2acbddf9066
Fernando Barreiro Megino <fh.barreiro@gmail.com>
2015-10-21 17:15:34 +0200

    Indexes on foreign keys

--- templates/DB/DB update scripts/configurator.sql
+++ templates/DB/DB update scripts/configurator.sql
@@ -15,6 +15,7 @@ constraint panda_site_name_pk primary key(panda_site_name),
 constraint site_fk foreign key(site_name) references atlas_panda.site(site_name),
 state varchar(20)
 )
+CREATE INDEX ATLAS_PANDA.panda_site_site_name ON ATLAS_PANDA.panda_site(site_name) COMPRESS 1;
 
 create table atlas_panda.ddm_endpoint
 (
@@ -25,6 +26,8 @@ constraint ddm_endpoint_name_pk primary key(ddm_endpoint_name),
 constraint ddm_site_fk foreign key(site_name) references atlas_panda.site(site_name),
 state varchar(20)
 )
+
+CREATE INDEX ATLAS_PANDA.ddm_endpoint_site_name ON ATLAS_PANDA.ddm_endpoint(site_name) COMPRESS 1;
 
 --m to n relationship between panda and ddm sites 
 create table atlas_panda.panda_ddm_relation
@@ -37,28 +40,4 @@ constraint panda_ddm_relation_pk primary key(panda_site_name, panda_ddm_name),
 constraint panda_site_fk foreign key(panda_site_name) references atlas_panda.panda_site(panda_site_name),
 constraint panda_ddm_endpoint_fk foreign key(panda_ddm_name) references atlas_panda.ddm_endpoint(ddm_endpoint_name)
 )
-
-select * from ATLAS_PANDA.site s, ATLAS_PANDA.panda_site ps, ATLAS_PANDA.ddm_endpoint de, ATLAS_PANDA.panda_ddm_relation pdr
-where s.site_name = ps.site_name
-and s.site_name = de.site_name
-and pdr.panda_site_name = ps.panda_site_name
-and pdr.ddm_endpoint_name = de.ddm_endpoint_name
-and s.site_name like '%HELIX%'
-
-select * from ATLAS_PANDA.site s
-where s.site_name like '%HELIX%'
-
-select * from ATLAS_PANDA.panda_site
-where site_name like '%HELIX%'
-
-select * from ATLAS_PANDA.ddm_endpoint
-where site_name like '%HELIX%'
-
-select * from ATLAS_PANDA.panda_ddm_relation
-order by 
-where panda_site_name like '%HELIX%'
-
-select * from ATLAS_PANDAMETA.schedconfig
-where nickname like '%Titan%'
-
 
