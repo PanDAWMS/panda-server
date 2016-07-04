f5763618f9d23e868caec5cb6f289241860aae91
Fernando Barreiro Megino <fh.barreiro@gmail.com>
2016-07-04 20:28:58 +0200

    Task-share matching for leave shares

--- pandaserver/taskbuffer/GlobalShares.py
+++ pandaserver/taskbuffer/GlobalShares.py
@@ -1,3 +1,6 @@
+from pandalogger.PandaLogger import PandaLogger
+_logger = PandaLogger().getLogger('TaskBuffer')
+
 class Node(object):
 
     def __init__(self):
@@ -28,7 +31,7 @@ class Share(Node):
         """
         Print the tree structure
         """
-        ret = "{0} name: {1} value: {2}\n".format('\t' * level, self.name, self.value)
+        ret = "{0} name: {1}, value: {2}\n".format('\t' * level, self.name, self.value)
         for child in self.children:
             ret += child.__str__(level + 1)
         return ret
@@ -62,7 +65,7 @@ class Share(Node):
         """
         Will run down the branch and normalize values beneath
         """
-        self.value *= (multiplier*1.0/divider)
+        self.value *= (multiplier * 1.0 / divider)
         if not self.children:
             return
 
@@ -85,37 +88,73 @@ class GlobalShares:
 
     def __init__(self, task_buffer):
 
-        self.task_buffer = task_buffer
+        self.__task_buffer = task_buffer
         # Root dummy node
         self.tree = Share('root', 100, None, None, None, None)
 
         # Get top level shares from DB
-        shares_top_level = self.task_buffer.getShares(parents=None)
+        shares_top_level = self.__task_buffer.getShares(parents=None)
 
         # Load branches
         for (name, value, parent, prodsourcelabel, workinggroup, campaign) in shares_top_level:
             share = Share(name, value, parent, prodsourcelabel, workinggroup, campaign)
-            self.tree.children.append(self.load_branch(share))
+            self.tree.children.append(self.__load_branch(share))
 
         # Normalize the values in the database
         self.tree.normalize()
 
-    def load_branch(self, share):
+    def __load_branch(self, share):
         """
         Recursively load a branch
         """
         node = Share(share.name, share.value, share.parent, share.prodsourcelabel, share.workinggroup, share.campaign)
 
-        children = self.task_buffer.getShares(parents=share.name)
+        children = self.__task_buffer.getShares(parents=share.name)
         if not children:
             return node
 
         for (name, value, parent, prodsourcelabel, workinggroup, campaign) in children:
             child = Share(name, value, parent, prodsourcelabel, workinggroup, campaign)
-            node.children.append(self.load_branch(child))
+            node.children.append(self.__load_branch(child))
 
         return node
 
+    def compare_share_task(self, share, task):
+        """
+        Logic to compare the relevant fields of share and task
+        """
+
+        if share.prodsourcelabel is not None and share.prodsourcelabel != task.prodSourceLabel:
+            return False
+
+        if share.workinggroup is not None and share.workinggroup != task.workingGroup:
+            return False
+
+        if share.campaign is not None and share.campaign != task.campaign:
+            return False
+
+        return True
+
+    def get_share_for_task(self, task):
+        """
+        Return the share based on a task specification
+        """
+
+        selected_share_name = None
+
+        # get the leave shares (the ones not having more children)
+        leave_shares = self.tree.get_leaves()
+
+        for share in leave_shares:
+            if self.compare_share_task(share, task):
+                selected_share_name = share.name
+                break
+
+        if selected_share_name is None:
+            self.__logger.warning("No share matching jediTaskId={0} (prodSourceLabel={1} workingGroup={2} campaign={3}".
+                    format(task.prodSourceLabel, task.workingGroup, task.campaign))
+
+        return selected_share_name
 
 if __name__ == "__main__":
     """
@@ -125,6 +164,11 @@ if __name__ == "__main__":
     from taskbuffer.TaskBuffer import taskBuffer
     from config import panda_config
 
-    taskBuffer.init(panda_config.dbhost,panda_config.dbpasswd,nDBConnection=1)
+    taskBuffer.init(panda_config.dbhost, panda_config.dbpasswd, nDBConnection=1)
     global_shares = GlobalShares(taskBuffer)
-    print global_shares.tree
+
+    # print the global share structure
+    print(global_shares.tree)
+
+    # print the normalized leaves, which will be the actual applied shares
+    print(global_shares.tree.get_leaves())
