e4651f7970157c8b26053634fe937713bcee51c5
Fernando Barreiro Megino <fh.barreiro@gmail.com>
2017-07-27 11:31:45 +0200

    adapted setokens in AdderAtlasPlugin to new structure

--- pandaserver/dataservice/AdderAtlasPlugin.py
+++ pandaserver/dataservice/AdderAtlasPlugin.py
@@ -255,9 +255,9 @@ class AdderAtlasPlugin (AdderPluginBase):
                                     # RSE is specified
                                     tmpDestList = [DataServiceUtils.getDestinationSE(file.destinationDBlockToken)]
                                 else:
-                                    if self.siteMapper.getSite(file.destinationSE).setokens.has_key(file.destinationDBlockToken):
+                                    if file.destinationDBlockToken in self.siteMapper.getSite(file.destinationSE).setokens_input:
                                         # get endpoint for token
-                                        tmpDestList = [self.siteMapper.getSite(file.destinationSE).setokens[file.destinationDBlockToken]]
+                                        tmpDestList = [self.siteMapper.getSite(file.destinationSE).setokens_input[file.destinationDBlockToken]]
                                     else:
                                         # use defalt endpoint
                                         tmpDestList = [self.siteMapper.getSite(file.destinationSE).ddm_input]
@@ -281,7 +281,7 @@ class AdderAtlasPlugin (AdderPluginBase):
                                 tmpDestList = [self.siteMapper.getSite(self.job.computingSite).ddm_output]
                             else:
                                 tmpDestList = []
-                                tmpSeTokens = self.siteMapper.getSite(self.job.computingSite).setokens
+                                tmpSeTokens = self.siteMapper.getSite(self.job.computingSite).setokens_input
                                 for tmpDestToken in file.destinationDBlockToken.split(','):
                                     if tmpSeTokens.has_key(tmpDestToken):
                                         tmpDest = tmpSeTokens[tmpDestToken]
@@ -389,16 +389,16 @@ class AdderAtlasPlugin (AdderPluginBase):
                                                 dq2ID = tmpSrcDDM
                                                 # use the first token's location as source for T1D1
                                                 tmpSrcToken = file.destinationDBlockToken.split(',')[0]
-                                                if self.siteMapper.getSite(self.job.computingSite).setokens.has_key(tmpSrcToken):
-                                                    dq2ID = self.siteMapper.getSite(self.job.computingSite).setokens[tmpSrcToken]
+                                                if tmpSrcToken in self.siteMapper.getSite(self.job.computingSite).setokens_output:
+                                                    dq2ID = self.siteMapper.getSite(self.job.computingSite).setokens_output[tmpSrcToken]
                                                 optSource[dq2ID] = {'policy' : 0}
                                             # T1 used as T2
                                             if self.siteMapper.getSite(self.job.computingSite).cloud != self.job.cloud and \
                                                (not tmpSrcDDM.endswith('PRODDISK')) and  \
                                                (not self.job.prodSourceLabel in ['user','panda']):
                                                 # register both DATADISK and PRODDISK as source locations
-                                                if self.siteMapper.getSite(self.job.computingSite).setokens.has_key('ATLASPRODDISK'):
-                                                    dq2ID = self.siteMapper.getSite(self.job.computingSite).setokens['ATLASPRODDISK']
+                                                if 'ATLASPRODDISK' in self.siteMapper.getSite(self.job.computingSite).setokens_output:
+                                                    dq2ID = self.siteMapper.getSite(self.job.computingSite).setokens_output['ATLASPRODDISK']
                                                     optSource[dq2ID] = {'policy' : 0}
                                                 if not optSource.has_key(tmpSrcDDM):
                                                     optSource[tmpSrcDDM] = {'policy' : 0}
@@ -412,8 +412,8 @@ class AdderAtlasPlugin (AdderPluginBase):
                                                 # loop over all tokens
                                                 for idxToken,tmpDstToken in enumerate(tmpDstTokens):
                                                     dq2ID = tmpDstDDM
-                                                    if self.siteMapper.getSite(file.destinationSE).setokens.has_key(tmpDstToken):
-                                                        dq2ID = self.siteMapper.getSite(file.destinationSE).setokens[tmpDstToken]
+                                                    if tmpDstToken in self.siteMapper.getSite(file.destinationSE).setokens_intput: # TODO: confirm with Tadashi
+                                                        dq2ID = self.siteMapper.getSite(file.destinationSE).setokens_intput[tmpDstToken]
                                                     # keep the fist destination for multi-hop
                                                     if idxToken == 0:
                                                         firstDestDDM = dq2ID
