c9e3fccca00d89e847c14d8ee2fbffd4fb55a679
fbarreir <fh.barreiro@gmail.com>
2017-09-05 13:04:49 +0200

    Handling of TEST endpoints

--- pandaserver/taskbuffer/DdmSpec.py
+++ pandaserver/taskbuffer/DdmSpec.py
@@ -17,20 +17,23 @@ class DdmSpec(object):
 
     # add endpoint
     def add(self, endPoint, endpointDict):
-        name = endPoint['ddm_endpoint_name']
-        # all endpoints
-        self.all[name] = endpointDict[name]
-        # local endpoints
-        if endPoint['is_local'] != 'N':
-            self.local.add(name)
-        # defaults
-        if endPoint['default_read'] == 'Y':
-            self.default_read = name
-        if endPoint['default_write'] == 'Y':
-            self.default_write = name
-        # tape
-        if endPoint['is_tape'] == 'Y':
-            self.tape.add(name)
+        try:
+            name = endPoint['ddm_endpoint_name']
+            # all endpoints
+            self.all[name] = endpointDict[name]
+            # local endpoints
+            if endPoint['is_local'] != 'N':
+                self.local.add(name)
+            # defaults
+            if endPoint['default_read'] == 'Y':
+                self.default_read = name
+            if endPoint['default_write'] == 'Y':
+                self.default_write = name
+            # tape
+            if endPoint['is_tape'] == 'Y':
+                self.tape.add(name)
+        except KeyError:
+            pass
 
     # get all endpoints
     def getAllEndPoints(self):
