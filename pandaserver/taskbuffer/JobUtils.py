2acf26b1ca0bc83d43574f0d76345c24555a1396
fbarreir <fh.barreiro@gmail.com>
2017-11-10 13:25:17 +0100

    Added HS06sec backwards compatibility for JEDI

--- pandaserver/taskbuffer/JobUtils.py
+++ pandaserver/taskbuffer/JobUtils.py
@@ -25,13 +25,18 @@ def getCoreCount(actualCoreCount, defCoreCount, jobMetrics):
 
 
 # get HS06sec
-def getHS06sec(startTime, endTime, corePower, coreCount):
+def getHS06sec(startTime, endTime, corePower, coreCount, baseWalltime=0, cpuEfficiency=100):
     try:
+        # no scaling
+        if cpuEfficiency == 0:
+            return 0
         # get execution time
         tmpTimeDelta = endTime-startTime
         tmpVal = tmpTimeDelta.seconds + tmpTimeDelta.days * 24 * 3600
-        tmpVal = tmpVal * corePower * coreCount
-        return tmpVal
+        if tmpVal <= baseWalltime:
+            return 0
+        hs06sec = float(tmpVal-baseWalltime) * corePower * coreCount
+        return hs06sec
     except:
         return None
     
