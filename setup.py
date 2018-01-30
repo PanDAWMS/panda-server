84479fca05fd8c23af238a1b782a9a46c09169ca
fbarreir <fh.barreiro@gmail.com>
2018-01-30 17:25:01 +0100

    Added pilot_streaming cron script to setup.py

--- setup.py
+++ setup.py
@@ -223,6 +223,7 @@ setup(
                              'templates/panda_server-configurator.exe.template',
                              'templates/panda_server-esPreemption.exe.template',
                              'templates/panda_server-harvesterCtl.exe.template',
+                             'templates/panda_server-pilot_streaming.exe.template'
                              ]
                  ),
 
