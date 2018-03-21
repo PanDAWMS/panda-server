79136b6e0380dc9411094ea499300475e33e35e7
tmaeno <tmaeno@bnl.gov>
2018-03-21 11:33:58 +0100

    added diskIO

--- pandaserver/taskbuffer/JobSpec.py
+++ pandaserver/taskbuffer/JobSpec.py
@@ -31,7 +31,9 @@ class JobSpec(object):
                    'jobSubStatus','actualCoreCount','reqID','maxRSS','maxVMEM','maxSWAP','maxPSS',
                    'avgRSS','avgVMEM','avgSWAP','avgPSS','maxWalltime','nucleus','eventService',
                    'failedAttempt','hs06sec', 'gshare', 'hs06','totRCHAR','totWCHAR','totRBYTES',
-                   'totWBYTES','rateRCHAR','rateWCHAR','rateRBYTES','rateWBYTES','resource_type')
+                   'totWBYTES','rateRCHAR','rateWCHAR','rateRBYTES','rateWBYTES','resource_type',
+                   'diskIO'
+                   )
     # slots
     __slots__ = _attributes+('Files','_changedAttrs')
     # attributes which have 0 by default
