f6972b6ba72138df25790da4d2eb134783f774e0
fbarreir <fh.barreiro@gmail.com>
2018-04-13 17:28:13 +0200

    UPS: changed print to log

--- pandaserver/test/pilotStreaming.py
+++ pandaserver/test/pilotStreaming.py
@@ -41,7 +41,7 @@ class PilotStreaming:
                 continue
 
             new_workers_per_harvester = taskBuffer.ups_new_worker_distribution(ups_queue, tmp_worker_stats)
-            print 'queue: {0}, results: {1}'.format(ups_queue, new_workers_per_harvester)
+            self._logger.info('queue: {0}, results: {1}'.format(ups_queue, new_workers_per_harvester))
 
             # variables for the harvester command
             command = '{0}:{1}'.format('SET_N_WORKERS', ups_queue)
