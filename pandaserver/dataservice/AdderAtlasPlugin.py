08d52bb25dc645d0b3c9a07f5177196281e8e3ea
tmaeno <tmaeno@bnl.gov>
2018-05-22 09:00:28 +0200

    * added guid check in Adder

--- pandaserver/dataservice/AdderAtlasPlugin.py
+++ pandaserver/dataservice/AdderAtlasPlugin.py
@@ -226,16 +226,10 @@ class AdderAtlasPlugin (AdderPluginBase):
                                 self.job.ddmErrorCode = ErrorCode.EC_MissingNumEvents
                                 raise ValueError, errMsg
                         if file.lfn not in self.extraInfo['guid'] or file.GUID != self.extraInfo['guid'][file.lfn]:
-                            toSkip = False
-                            # exclude some formats
-                            for patt in ['TXT', 'NTUP', 'HIST']:
-                                if file.lfn.startswith(patt):
-                                    toSkip = True
-                            if not toSkip:
-                                errMsg = "GUID is inconsistent between jobReport and pilot report for {0}".format(file.lfn)
-                                self.logger.warning(errMsg)
-                                #self.job.ddmErrorCode = ErrorCode.EC_InconsistentGUID
-                                #raise ValueError, errMsg
+                            errMsg = "GUID is inconsistent between jobReport and pilot report for {0}".format(file.lfn)
+                            self.logger.warning(errMsg)
+                            self.job.ddmErrorCode = ErrorCode.EC_InconsistentGUID
+                            raise ValueError, errMsg
                     # fsize
                     fsize = None
                     if not file.fsize in ['NULL','',0]:
