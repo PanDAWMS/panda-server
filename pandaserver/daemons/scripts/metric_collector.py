8de229c9e46d733544d7678e17694dd505eab4db
mightqxc <fahui.lin@cern.ch>
2021-11-09 19:48:50 +0800

    metric collector: add metric lock, fix

--- pandaserver/daemons/scripts/metric_collector.py
+++ pandaserver/daemons/scripts/metric_collector.py
@@ -1,3 +1,5 @@
+import os
+import socket
 import time
 import datetime
 import json
@@ -5,6 +7,8 @@ import functools
 import traceback
 import copy
 import statistics
+
+from zlib import adler32
 
 from pandacommon.pandalogger.PandaLogger import PandaLogger
 from pandacommon.pandalogger import logger_utils
@@ -16,9 +20,9 @@ from pandaserver.config import panda_config
 main_logger = PandaLogger().getLogger('metric_collector')
 
 
-# list of metrics in FetchData to fetch data and update to DB
+# list of metrics in FetchData to fetch data and update to DB. Format: (metric, type, period_sec)
 metric_list = [
-    ('analy_pmerge_jobs_wait_time', 'site'),
+    ('analy_pmerge_jobs_wait_time', 'site', 1800),
 ]
 
 
@@ -52,7 +56,7 @@ class MetricsDB(object):
 
     def update(self, key, value, site, gshare):
         tmp_log = logger_utils.make_logger(main_logger, 'MetricsDB')
-        tmp_log.debug('start key={0} site={1}, gshare={2}'.format(key, site, gshare))
+        # tmp_log.debug('start key={0} site={1}, gshare={2}'.format(key, site, gshare))
         # sql
         sql_update_tmp = (
             """UPDATE ATLAS_PANDA.Metrics SET """
@@ -98,7 +102,7 @@ class MetricsDB(object):
         else:
             tmp_log.debug('updated site={site}, gshare={gshare}'.format(site=site, gshare=gshare))
         # done
-        tmp_log.debug('done key={0} site={1}, gshare={2}'.format(key, site, gshare))
+        # tmp_log.debug('done key={0} site={1}, gshare={2}'.format(key, site, gshare))
 
     def update_site(self, key, value, site):
         return self.update(key, value, site=site, gshare='NULL')
@@ -117,6 +121,7 @@ class FetchData(object):
 
     def analy_pmerge_jobs_wait_time(self):
         tmp_log = logger_utils.make_logger(main_logger, 'FetchData')
+        #sql
         sql_get_jobs = (
             "SELECT pandaID, computingSite "
             "FROM ATLAS_PANDA.jobsArchived4 "
@@ -132,10 +137,13 @@ class FetchData(object):
         )
         try:
             # initialize
-            site_dict = dict()
+            tmp_site_dict = dict()
             # get user jobs
             jobs_list = self.tbuf.querySQL(sql_get_jobs, {})
+            n_tot_jobs = len(jobs_list)
+            tmp_log.debug('got total {0} jobs'.format(n_tot_jobs))
             # loop over jobs to get modificationTime when activated and running
+            cc = 0
             for pandaID, site in jobs_list:
                 if not site:
                     continue
@@ -149,13 +157,101 @@ class FetchData(object):
                 if wait_time_sec < 0:
                     tmp_log.warning('job {0} has negative wait time'.format(pandaID))
                     continue
-                site_dict.setdefault(site, {})
-                site_dict[site].setdefault('data', [])
-                site_dict[site]['data'].append(wait_time_sec)
+                tmp_site_dict.setdefault(site, [])
+                tmp_site_dict[site].append(wait_time_sec)
+                # log message
+                if cc > 0 and cc % 5000 == 0:
+                    tmp_log.debug('... queried {0:9d} jobs ...'.format(cc))
+                cc += 1
+            tmp_log.debug('queried {0} jobs'.format(cc))
             # evaluate stats
-            site_dict_copy = copy.deepcopy(site_dict)
-            for site, v in site_dict_copy.items():
-                data_list = v['data']
+            site_dict = dict()
+            for site, data_list in tmp_site_dict.items():
+                site_dict.setdefault(site, {})
+                n_jobs = len(data_list)
+                try:
+                    mean = statistics.mean(data_list)
+                except statistics.StatisticsError:
+                    mean = None
+                try:
+                    stdev = statistics.stdev(data_list)
+                except statistics.StatisticsError:
+                    stdev = None
+                try:
+                    stdev = statistics.stdev(data_list)
+                except statistics.StatisticsError:
+                    stdev = None
+                try:
+                    median = statistics.median(data_list)
+                except statistics.StatisticsError:
+                    median = median
+                # try:
+                #     quantiles = statistics.quantiles(data_list, n=4, method='inclusive')
+                # except statistics.StatisticsError:
+                #     quantiles = None
+                # update
+                site_dict[site].update({
+                        'n': n_jobs,
+                        'mean': mean,
+                        'stdev': stdev,
+                        'med': median,
+                        # 'quantiles': quantiles,
+                    })
+                tmp_log.debug('site={site}, n={n}, mean={mean:.3f}, stdev={stdev:.3f}, med={med:.3f}'.format(site=site, **site_dict[site]))
+            # return
+            return site_dict
+        except Exception:
+            tmp_log.error(traceback.format_exc())
+
+    def gshare_preference(self):
+        tmp_log = logger_utils.make_logger(main_logger, 'FetchData')
+        # sql
+        sql_get_jobs = (
+            "SELECT pandaID, computingSite "
+            "FROM ATLAS_PANDA.jobsArchived4 "
+            "WHERE prodSourceLabel='user' "
+                "AND jobStatus='finished' "
+                "AND processingType='pmerge' "
+        )
+        sql_get_latest_job_mtime_status = (
+            "SELECT jobStatus, MIN(modificationTime) "
+            "FROM ATLAS_PANDA.jobs_StatusLog "
+            "WHERE pandaID=:pandaID "
+            "GROUP BY jobStatus "
+        )
+        try:
+            # initialize
+            tmp_site_dict = dict()
+            # get user jobs
+            jobs_list = self.tbuf.querySQL(sql_get_jobs, {})
+            n_tot_jobs = len(jobs_list)
+            tmp_log.debug('got total {0} jobs'.format(n_tot_jobs))
+            # loop over jobs to get modificationTime when activated and running
+            cc = 0
+            for pandaID, site in jobs_list:
+                if not site:
+                    continue
+                varMap = {':pandaID': pandaID}
+                status_mtime_list = self.tbuf.querySQL(sql_get_latest_job_mtime_status, varMap)
+                status_mtime_dict = dict(status_mtime_list)
+                if 'activated' not in status_mtime_dict or 'running' not in status_mtime_dict:
+                    continue
+                wait_time = status_mtime_dict['running'] - status_mtime_dict['activated']
+                wait_time_sec = wait_time.total_seconds()
+                if wait_time_sec < 0:
+                    tmp_log.warning('job {0} has negative wait time'.format(pandaID))
+                    continue
+                tmp_site_dict.setdefault(site, [])
+                tmp_site_dict[site].append(wait_time_sec)
+                # log message
+                if cc > 0 and cc % 5000 == 0:
+                    tmp_log.debug('... queried {0:9d} jobs ...'.format(cc))
+                cc += 1
+            tmp_log.debug('queried {0} jobs'.format(cc))
+            # evaluate stats
+            site_dict = dict()
+            for site, data_list in tmp_site_dict.items():
+                site_dict.setdefault(site, {})
                 n_jobs = len(data_list)
                 try:
                     mean = statistics.mean(data_list)
@@ -175,10 +271,12 @@ class FetchData(object):
                 #     quantiles = None
                 # update
                 site_dict[site].update({
+                        'n': n_jobs,
                         'mean': mean,
                         'stdev': stdev,
                         # 'quantiles': quantiles,
                     })
+                tmp_log.debug('site={site}, n={n}, mean={mean:.3f}, stdev={stdev:.3f}'.format(site=site, **site_dict[site]))
             # return
             return site_dict
         except Exception:
@@ -193,12 +291,25 @@ def main(tbuf=None, **kwargs):
         taskBuffer.init(panda_config.dbhost, panda_config.dbpasswd, nDBConnection=1)
     else:
         taskBuffer = tbuf
+    # pid
+    my_pid = os.getpid()
+    my_full_pid = '{0}-{1}-{2}'.format(socket.getfqdn().split('.')[0], os.getpgrp(), my_pid)
     # instantiate
     mdb = MetricsDB(taskBuffer)
     fetcher = FetchData(taskBuffer)
     # loop over all fetch data methods to run and update to DB
-    for metric_name, update_type in metric_list:
+    for metric_name, update_type, period in metric_list:
+        # metric lock
+        lock_component_name = 'pandaMetr.{0:.30}.{1:0x}'.format(metric_name, adler32(metric_name.encode('utf-8')))
+        # try to get lock
+        got_lock = taskBuffer.lockProcess_PANDA(component=lock_component_name, pid=my_full_pid, time_limit=period)
+        if got_lock:
+            main_logger.debug('got lock of {metric_name}'.format(metric_name=metric_name))
+        else:
+            main_logger.debug('{metric_name} locked by other process; skipped...'.format(metric_name=metric_name))
+            continue
         main_logger.debug('start {metric_name}'.format(metric_name=metric_name))
+        # fetch data and update DB
         the_method = getattr(fetcher, metric_name)
         fetched_data = the_method()
         if fetched_data is None:
