032d00a3abe6c362f279798d8c1f34626265bb90
fbarreir <fh.barreiro@gmail.com>
2021-11-16 10:03:48 +0100

    added logger to sendCommand

--- pandaserver/test/testUpdateWorkerPilotStatus.py
+++ pandaserver/test/testUpdateWorkerPilotStatus.py
@@ -1,11 +1,15 @@
 from pandaserver.test.testutils import sendCommand
 
+from pandacommon.pandalogger.PandaLogger import PandaLogger
+_logger = PandaLogger().getLogger('testJobFlowATLAS')
+
 node = {}
 node['workerID'] = 9139456
 node['harvesterID'] = 'CERN_central_k8s'
 node['status'] = 'started'
 
 function = "updateWorkerPilotStatus"
-data = sendCommand(function, node)
-print(data)
+data = sendCommand(function, node, _logger)
+_logger.debug(data)
+
 
