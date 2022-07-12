075092f8e549f1f613fac6f3966982e821319072
fbarreir <fh.barreiro@gmail.com>
2022-07-12 13:44:37 +0200

    automated reformatting

--- pandaserver/taskbuffer/WorkerSpec.py
+++ pandaserver/taskbuffer/WorkerSpec.py
@@ -13,8 +13,8 @@ except NameError:
 
 class WorkerSpec(object):
     # attributes
-    _attributes = ('harvesterID','workerID','batchID','queueName','status','computingSite','nCore',
-                   'nodeID','submitTime','startTime','endTime','lastUpdate', 'stdOut', 'stdErr',
+    _attributes = ('harvesterID', 'workerID', 'batchID', 'queueName', 'status', 'computingSite', 'nCore',
+                   'nodeID', 'submitTime', 'startTime', 'endTime', 'lastUpdate', 'stdOut', 'stdErr',
                    'batchLog', 'jdl', 'resourceType', 'nativeExitCode', 'nativeStatus', 'diagMessage',
                    'nJobs', 'computingElement', 'submissionHost', 'harvesterHost', 'errorCode', 'jobType')
     # slots
@@ -24,56 +24,49 @@ class WorkerSpec(object):
     # catchall resouce type
     RT_catchall = 'ANY'
 
-
     # constructor
     def __init__(self):
         # install attributes
         for attr in self._attributes:
-            object.__setattr__(self,attr,None)
+            object.__setattr__(self, attr, None)
         # map of changed attributes
-        object.__setattr__(self,'_changedAttrs',{})
-
+        object.__setattr__(self, '_changedAttrs', {})
 
     # override __setattr__ to collecte the changed attributes
-    def __setattr__(self,name,value):
-        oldVal = getattr(self,name)
+    def __setattr__(self, name, value):
+        oldVal = getattr(self, name)
         # convert string to datetime
-        if type(value) in [str,unicode] and value.startswith('datetime/'):
-            value = datetime.datetime.strptime(value.split('/')[-1],'%Y-%m-%d %H:%M:%S.%f')
-        object.__setattr__(self,name,value)
+        if type(value) in [str, unicode] and value.startswith('datetime/'):
+            value = datetime.datetime.strptime(value.split('/')[-1], '%Y-%m-%d %H:%M:%S.%f')
+        object.__setattr__(self, name, value)
         # collect changed attributes
         if oldVal != value:
             self._changedAttrs[name] = value
 
-
     # reset changed attribute list
     def resetChangedList(self):
         self._oldPandaID = self.PandaID
-        object.__setattr__(self,'_changedAttrs',{})
-
+        object.__setattr__(self, '_changedAttrs', {})
 
     # return map of values
-    def valuesMap(self,onlyChanged=False):
+    def valuesMap(self, onlyChanged=False):
         ret = {}
         for attr in self._attributes:
             if onlyChanged and attr not in self._changedAttrs:
                 continue
-            val = getattr(self,attr)
+            val = getattr(self, attr)
             if val is None:
                 if attr in self._zeroAttrs:
                     val = 0
             ret[':%s' % attr] = val
         return ret
 
-
     # pack tuple into FileSpec
-    def pack(self,values):
+    def pack(self, values):
         for i in range(len(self._attributes)):
-            attr= self._attributes[i]
+            attr = self._attributes[i]
             val = values[i]
-            object.__setattr__(self,attr,val)
-
-
+            object.__setattr__(self, attr, val)
 
     # return column names for INSERT
     def columnNames(cls, prefix=None):
@@ -82,10 +75,10 @@ class WorkerSpec(object):
             if prefix is not None:
                 ret += '{0}.'.format(prefix)
             ret += '{0},'.format(attr)
-        ret  = ret[:-1]
+        ret = ret[:-1]
         return ret
-    columnNames = classmethod(columnNames)
 
+    columnNames = classmethod(columnNames)
 
     # return expression of bind variables for INSERT
     def bindValuesExpression(cls):
@@ -96,8 +89,8 @@ class WorkerSpec(object):
         ret = ret[:-1]
         ret += ")"
         return ret
-    bindValuesExpression = classmethod(bindValuesExpression)
 
+    bindValuesExpression = classmethod(bindValuesExpression)
 
     # return an expression of bind variables for UPDATE to update only changed attributes
     def bindUpdateChangesExpression(self):
@@ -106,5 +99,5 @@ class WorkerSpec(object):
             if attr not in self._changedAttrs:
                 continue
             ret += '{0}=:{0},'.format(attr)
-        ret  = ret[:-1]
+        ret = ret[:-1]
         return ret
