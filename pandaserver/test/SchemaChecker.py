d7d9195a11f9148e49e00b225fa3b1e939d2a7fd
Edward Karavakis <edward.karavakis@cern.ch>
2022-11-07 15:20:57 +0100

    Sending an email if the pandaEmailNotification var is defined in the panda config file.

--- pandaserver/test/SchemaChecker.py
+++ pandaserver/test/SchemaChecker.py
@@ -1,10 +1,12 @@
 """
-checking DB schema version for PanDA Server
-
+Checking DB schema version for PanDA Server.
+If there is an issue and the pandaEmailNotification var is
+defined in panda_config, it will send an email notification.
 """
 
 from pandaserver.config import panda_config
 from pandaserver.taskbuffer.OraDBProxy import DBProxy
+from pandaserver.srvcore.MailUtils import MailUtils
 
 from pandaserver.taskbuffer import PandaDBSchemaInfo
 
@@ -24,3 +26,9 @@ if version.parse(dbVersion) >= version.parse(serverDBVersion):
     return ("True")
 else:
     return ("False")
+    if 'pandaEmailNotification' in panda_config.__dict__:
+        msgBody = 'There is an issue with ' + panda_config.pserveralias + '. PanDA DB schema installed is ' + dbVersion + \
+                  ' while PanDA Server requires version ' + serverDBVersion + \
+                  ' to be installed. Please check the official docs for instructions on how to upgrade the schema.'
+        MailUtils().send(panda_config.pandaEmailNotification,
+                         'PanDA DB Version installed is not correct for ' + panda_config.pserveralias, msgBody)
