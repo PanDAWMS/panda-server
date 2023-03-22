cde619b757489276e0e8e667ed7c333d5a0ce7ea
tmaeno <tmaeno@bnl.gov>
2023-03-22 10:20:17 +0100

    recover CloudSpec.py

---
+++ pandaserver/taskbuffer/CloudSpec.py
@@ -0,0 +1,24 @@
+"""
+cloud specification
+
+"""
+
+
+class CloudSpec(object):
+    # attributes
+    _attributes = ('name', 'tier1', 'tier1SE', 'relocation', 'weight', 'server', 'status', 'transtimelo',
+                   'transtimehi', 'waittime', 'validation', 'mcshare', 'countries', 'fasttrack', 'nprestage',
+                   'pilotowners')
+
+    # constructor
+    def __init__(self):
+        # install attributes
+        for attr in self._attributes:
+            setattr(self, attr, None)
+
+    # serialize
+    def __str__(self):
+        str = ''
+        for attr in self._attributes:
+            str += '%s:%s ' % (attr, getattr(self, attr))
+        return str
