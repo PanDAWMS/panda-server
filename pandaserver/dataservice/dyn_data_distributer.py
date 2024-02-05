e1066bb7c4da95aedea0fe54403d248b73fcf54f
Edward Karavakis <edward.karavakis@cern.ch>
2024-02-05 14:11:33 +0100

    Increase debugging a bit

--- pandaserver/dataservice/dyn_data_distributer.py
+++ pandaserver/dataservice/dyn_data_distributer.py
@@ -639,8 +639,8 @@ class DynDataDistributer:
                     tmp_logger.debug(f"use the first dataset in {str(tmp_dataset_names)} for GUID:{guid}")
 
                 ret_map[guid] = tmp_dataset_names[0]
-        except Exception:
-            tmp_logger.error("failed to parse listDatasetsByGUIDs")
+        except Exception as e:
+            tmp_logger.error(f"failed to parse get_datasets_by_guids: {e}")
             tmp_logger.debug("end")
             return False, {}
 
