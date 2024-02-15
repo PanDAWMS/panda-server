808837c277886dfa116e2e93ea0d89394834dc04
Edward Karavakis <edward.karavakis@cern.ch>
2024-02-15 18:19:04 +0100

    Reverting back previous logic

--- pandaserver/dataservice/finisher.py
+++ pandaserver/dataservice/finisher.py
@@ -161,92 +161,93 @@ class Finisher(threading.Thread):
                 jobs = [self.job]
                 # loop over all jobs
                 for job in jobs:
-                    if job == "transferring":
-                        job_ready, failed_files, no_out_files = self.check_file_status(job)
-                        # finish job
-                        if job_ready:
-                            tmp_log.debug(f"Job: {job.PandaID} all files checked with catalog")
-                            # # create JSON
-                            try:
-                                self.update_job_output_report(job, failed_files, no_out_files)
-                            except Exception:
-                                exc_type, value, _ = sys.exc_info()
-                                tmp_log.error(f"Job: {job.PandaID} {exc_type} {value}")
-                            # create XML
-                            # try:
-                            #     import xml.dom.minidom
-                            #
-                            #     dom = xml.dom.minidom.getDOMImplementation()
-                            #     doc = dom.createDocument(None, "xml", None)
-                            #     top_node = doc.createElement("POOLFILECATALOG")
-                            #     for file in job.Files:
-                            #         if file.type in ["output", "log"]:
-                            #             # skip failed or no-output files
-                            #             if file.lfn in failed_files + no_out_files:
-                            #                 continue
-                            #             # File
-                            #             file_node = doc.createElement("File")
-                            #             file_node.setAttribute("ID", file.GUID)
-                            #             # LFN
-                            #             log_node = doc.createElement("logical")
-                            #             lfn_node = doc.createElement("lfn")
-                            #             lfn_node.setAttribute("name", file.lfn)
-                            #             # metadata
-                            #             fsize_node = doc.createElement("metadata")
-                            #             fsize_node.setAttribute("att_name", "fsize")
-                            #             fsize_node.setAttribute("att_value", str(file.fsize))
-                            #             # checksum
-                            #             if file.checksum.startswith("ad:"):
-                            #                 # adler32
-                            #                 checksum_node = doc.createElement("metadata")
-                            #                 checksum_node.setAttribute("att_name", "adler32")
-                            #                 checksum_node.setAttribute(
-                            #                     "att_value",
-                            #                     re.sub("^ad:", "", file.checksum),
-                            #                 )
-                            #             else:
-                            #                 # md5sum
-                            #                 checksum_node = doc.createElement("metadata")
-                            #                 checksum_node.setAttribute("att_name", "md5sum")
-                            #                 checksum_node.setAttribute(
-                            #                     "att_value",
-                            #                     re.sub("^md5:", "", file.checksum),
-                            #                 )
-                            #             # append nodes
-                            #             log_node.appendChild(lfn_node)
-                            #             file_node.appendChild(log_node)
-                            #             file_node.appendChild(fsize_node)
-                            #             file_node.appendChild(checksum_node)
-                            #             top_node.appendChild(file_node)
-                            #     # status of the job record
-                            #     if not failed_files:
-                            #         record_status = "finished"
-                            #     else:
-                            #         record_status = "failed"
-                            #     # write to file
-                            #     # xmlFile = '%s/%s_%s_%s' % (panda_config.logdir,job.PandaID,record_status,
-                            #     #                            str(uuid.uuid4()))
-                            #     # oXML = open(xmlFile,"w")
-                            #     # oXML.write(top_node.toxml())
-                            #     # oXML.close()
-                            #     # write to job output report table, try update first
-                            #     tmp_ret = self.task_buffer.updateJobOutputReport(
-                            #         panda_id=job.PandaID,
-                            #         attempt_nr=job.attemptNr,
-                            #         data=top_node.toxml(),
-                            #     )
-                            #     if not tmp_ret:
-                            #         # then try insert
-                            #         self.task_buffer.insertJobOutputReport(
-                            #             panda_id=job.PandaID,
-                            #             prod_source_label=job.prodSourceLabel,
-                            #             job_status=record_status,
-                            #             attempt_nr=job.attemptNr,
-                            #             data=top_node.toxml(),
-                            #         )
-                            # except Exception:
-                            #     exc_type, value, _ = sys.exc_info()
-                            #     tmp_log.error(f"Job: {job.PandaID} {exc_type} {value}")
+                    if job is None or job.jobStatus != "transferring":
+                        continue
+                    job_ready, failed_files, no_out_files = self.check_file_status(job)
+                    # finish job
+                    if job_ready:
+                        tmp_log.debug(f"Job: {job.PandaID} all files checked with catalog")
+                        # # create JSON
+                        try:
+                            self.update_job_output_report(job, failed_files, no_out_files)
+                        except Exception:
+                            exc_type, value, _ = sys.exc_info()
+                            tmp_log.error(f"Job: {job.PandaID} {exc_type} {value}")
+                        # create XML
+                        # try:
+                        #     import xml.dom.minidom
+                        #
+                        #     dom = xml.dom.minidom.getDOMImplementation()
+                        #     doc = dom.createDocument(None, "xml", None)
+                        #     top_node = doc.createElement("POOLFILECATALOG")
+                        #     for file in job.Files:
+                        #         if file.type in ["output", "log"]:
+                        #             # skip failed or no-output files
+                        #             if file.lfn in failed_files + no_out_files:
+                        #                 continue
+                        #             # File
+                        #             file_node = doc.createElement("File")
+                        #             file_node.setAttribute("ID", file.GUID)
+                        #             # LFN
+                        #             log_node = doc.createElement("logical")
+                        #             lfn_node = doc.createElement("lfn")
+                        #             lfn_node.setAttribute("name", file.lfn)
+                        #             # metadata
+                        #             fsize_node = doc.createElement("metadata")
+                        #             fsize_node.setAttribute("att_name", "fsize")
+                        #             fsize_node.setAttribute("att_value", str(file.fsize))
+                        #             # checksum
+                        #             if file.checksum.startswith("ad:"):
+                        #                 # adler32
+                        #                 checksum_node = doc.createElement("metadata")
+                        #                 checksum_node.setAttribute("att_name", "adler32")
+                        #                 checksum_node.setAttribute(
+                        #                     "att_value",
+                        #                     re.sub("^ad:", "", file.checksum),
+                        #                 )
+                        #             else:
+                        #                 # md5sum
+                        #                 checksum_node = doc.createElement("metadata")
+                        #                 checksum_node.setAttribute("att_name", "md5sum")
+                        #                 checksum_node.setAttribute(
+                        #                     "att_value",
+                        #                     re.sub("^md5:", "", file.checksum),
+                        #                 )
+                        #             # append nodes
+                        #             log_node.appendChild(lfn_node)
+                        #             file_node.appendChild(log_node)
+                        #             file_node.appendChild(fsize_node)
+                        #             file_node.appendChild(checksum_node)
+                        #             top_node.appendChild(file_node)
+                        #     # status of the job record
+                        #     if not failed_files:
+                        #         record_status = "finished"
+                        #     else:
+                        #         record_status = "failed"
+                        #     # write to file
+                        #     # xmlFile = '%s/%s_%s_%s' % (panda_config.logdir,job.PandaID,record_status,
+                        #     #                            str(uuid.uuid4()))
+                        #     # oXML = open(xmlFile,"w")
+                        #     # oXML.write(top_node.toxml())
+                        #     # oXML.close()
+                        #     # write to job output report table, try update first
+                        #     tmp_ret = self.task_buffer.updateJobOutputReport(
+                        #         panda_id=job.PandaID,
+                        #         attempt_nr=job.attemptNr,
+                        #         data=top_node.toxml(),
+                        #     )
+                        #     if not tmp_ret:
+                        #         # then try insert
+                        #         self.task_buffer.insertJobOutputReport(
+                        #             panda_id=job.PandaID,
+                        #             prod_source_label=job.prodSourceLabel,
+                        #             job_status=record_status,
+                        #             attempt_nr=job.attemptNr,
+                        #             data=top_node.toxml(),
+                        #         )
+                        # except Exception:
+                        #     exc_type, value, _ = sys.exc_info()
+                        #     tmp_log.error(f"Job: {job.PandaID} {exc_type} {value}")
 
                     tmp_log.debug(f"Job: {job.PandaID} status: {job.jobStatus}")
             tmp_log.debug(f"end: {self.job.PandaID}")
