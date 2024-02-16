1a918939a8bc736b4216fec863ad040a829eb220
Fernando Harald Barreiro Megino <fh.barreiro@gmail.com>
2024-02-16 16:14:08 +0100

    Import sys

--- pandaserver/proxycache/panda_activeusers_query.py
+++ pandaserver/proxycache/panda_activeusers_query.py
@@ -1,18 +1,20 @@
 import re
+import sys
 
 from pandacommon.pandalogger.LogWrapper import LogWrapper
 from pandacommon.pandalogger.PandaLogger import PandaLogger
 from pandacommon.pandautils.thread_utils import GenericThread
+
 from pandaserver.config import panda_config
 from pandaserver.proxycache import panda_proxy_cache
 from pandaserver.taskbuffer.TaskBuffer import taskBuffer
 
 # logger
-_logger = PandaLogger().getLogger("panda_activeusers_query")
-tmpLog = LogWrapper(_logger)
+_logger = PandaLogger().getLogger("activeusers_query")
+tmp_log = LogWrapper(_logger)
 
 if __name__ == "__main__":
-    tmpLog.debug("================= start ==================")
+    tmp_log.debug("================= start ==================")
 
     requester_id = GenericThread().get_full_id(__name__, sys.modules[__name__].__file__)
     taskBuffer.init(
@@ -34,7 +36,7 @@ if __name__ == "__main__":
     sql = "select distinct DN FROM ATLAS_PANDAMETA.users WHERE GRIDPREF LIKE :patt"
     varMap = {}
     varMap[":patt"] = "%p%"
-    tmpStat, tmpRes = taskBuffer.querySQLS(sql, varMap)
+    _, tmpRes = taskBuffer.querySQLS(sql, varMap)
     for (realDN,) in tmpRes:
         if realDN is None:
             continue
@@ -42,7 +44,7 @@ if __name__ == "__main__":
         realDN = re.sub("(/CN=proxy)+", "", realDN)
         realDN = re.sub("(/CN=\d+)+$", "", realDN)
         # check proxy
-        tmpLog.debug(f"check proxy cache for DN={realDN}")
+        tmp_log.debug(f"check proxy cache for DN={realDN}")
         for role in roles:
             my_proxy_interface_instance.checkProxy(realDN, role=role)
-    tmpLog.debug("done")
+    tmp_log.debug("done")
