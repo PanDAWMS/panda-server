90140ece9c2cf1c119648afc07378059615c5544
Edward Karavakis <edward.karavakis@cern.ch>
2024-02-26 14:31:36 +0100

    Add protection when job is None.

--- pandaserver/dataservice/finisher.py
+++ pandaserver/dataservice/finisher.py
@@ -152,11 +152,13 @@ class Finisher(threading.Thread):
                              f"run-{datetime.datetime.now(datetime.timezone.utc).replace(tzinfo=None).isoformat('/')}")
         # start
         try:
+            by_call_back = False
             if self.job is not None:
                 tmp_log.debug(f"start: {self.job.PandaID}")
                 panda_ids = [self.job.PandaID]
                 jobs = [self.job]
             else:
+                by_call_back = True
                 tmp_log.debug(f"start: {self.dataset.name}")
                 panda_ids = self.task_buffer.updateOutFilesReturnPandaIDs(self.dataset.name)
                 # set flag for T2 cleanup
@@ -173,7 +175,10 @@ class Finisher(threading.Thread):
                     job_ready, failed_files, no_out_files = self.check_file_status(job)
                     # finish job
                     if job_ready:
-                        tmp_log.debug(f"Job: {job.PandaID} all files checked with catalog")
+                        if by_call_back:
+                            tmp_log.debug(f"Job: {job.PandaID} all files ready")
+                        else:
+                            tmp_log.debug(f"Job: {job.PandaID} all files checked with catalog")
                         # create JSON
                         try:
                             self.update_job_output_report(job, failed_files, no_out_files)
@@ -181,7 +186,10 @@ class Finisher(threading.Thread):
                             exc_type, value, _ = sys.exc_info()
                             tmp_log.error(f"Job: {job.PandaID} {exc_type} {value}")
                     tmp_log.debug(f"Job: {job.PandaID} status: {job.jobStatus}")
-            tmp_log.debug(f"end: {self.job.PandaID}")
+            if self.job is None:
+                tmp_log.debug(f"end: {self.dataset.name}")
+            else:
+                tmp_log.debug(f"end: {self.job.PandaID}")
         except Exception:
             exc_type, value, _ = sys.exc_info()
             tmp_log.error(f"run() : {exc_type} {value}")
