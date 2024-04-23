7b75570e819b0dec8a95efd7601adf5d7eca1d8c
tmaeno <tmaeno@bnl.gov>
2024-04-23 12:58:47 +0200

    to reassign high priority jobs if a PQ is in test status for 4hrs, rgardless of laststart

--- pandaserver/daemons/scripts/copyArchive.py
+++ pandaserver/daemons/scripts/copyArchive.py
@@ -630,7 +630,7 @@ def main(argv=tuple(), tbuf=None, **kwargs):
     varMap[":rFlag1"] = 2
     stDS, resDS = taskBuffer.querySQLS(sql, varMap)
     sqlSS = "SELECT laststart FROM ATLAS_PANDAMETA.siteData "
-    sqlSS += "WHERE site=:site AND flag=:flag AND hours=:hours AND laststart<:laststart "
+    sqlSS += "WHERE site=:site AND flag=:flag AND hours=:hours "
     sqlPI = "SELECT PandaID,eventService,attemptNr FROM ATLAS_PANDA.jobsActive4 "
     sqlPI += "WHERE prodSourceLabel=:prodSourceLabel AND jobStatus IN (:jobStatus1,:jobStatus2) "
     sqlPI += "AND (modificationTime<:timeLimit OR stateChangeTime<:timeLimit) "
@@ -645,14 +645,13 @@ def main(argv=tuple(), tbuf=None, **kwargs):
         varMap[":site"] = tmpSite
         varMap[":flag"] = "production"
         varMap[":hours"] = 3
-        varMap[":laststart"] = timeLimitSite
         stSS, resSS = taskBuffer.querySQLS(sqlSS, varMap)
-        if len(resSS) > 0:
+        if resSS is not None and len(resSS) > 0:
             last_start = resSS[0][0]
         else:
             last_start = None
         site_status = siteMapper.getSite(tmpSite).status
-        if stSS is True and (last_start is not None or site_status in ["offline", "test"]):
+        if stSS is True and (last_start is None or last_start < timeLimitSite or site_status in ["offline", "test"]):
             # get jobs
             varMap = {}
             varMap[":prodSourceLabel"] = "managed"
