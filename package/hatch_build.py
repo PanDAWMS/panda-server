ae975a99e9bdf115b0a9311217da51f49b9066e7
fbarreir <fh.barreiro@gmail.com>
2024-10-02 12:48:13 +0200

    Trying

--- package/hatch_build.py
+++ package/hatch_build.py
@@ -52,7 +52,7 @@ def mm_notification():
             return
 
     mm_message = {
-        "text": f"⚙️**Install Information.** **Package:** \"{repo_name[:7]}\u200B{repo_name[7:]}\". **Server Name:** {server_name}. **Branch:** {branch_name}. **Commit:** {commit_hash}."
+        "text": f"⚙️**Install Information.** **Server Name:** {server_name}. **Package:** \"{repo_name[:7]}\u200B{repo_name[7:]}\". **Branch:** {branch_name}. **Commit:** {commit_hash}."
     }
     headers = {'Content-Type': 'application/json'}
     try:
