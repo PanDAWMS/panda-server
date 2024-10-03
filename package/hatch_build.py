284d92b4be5bc40e65afe189e15ee30aef7d3ff9
fbarreir <fh.barreiro@gmail.com>
2024-10-03 10:09:56 +0200

    Replace rstrip by removesuffix and other corrections on hatch_build.py

--- package/hatch_build.py
+++ package/hatch_build.py
@@ -6,63 +6,58 @@ import json
 import os
 import pwd
 import re
-import requests
 import socket
 import stat
 import subprocess
 import sys
 import sysconfig
 
-
+import requests
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 
 def get_repo_info() -> object:
     # Get the current remote URL of the repository
-    repo_url = subprocess.check_output(['git', 'config', '--get', 'remote.origin.url']).strip().decode()
+    repo_url = subprocess.check_output(["git", "config", "--get", "remote.origin.url"]).strip().decode()
 
     # Get the repo and branch name
-    match = re.match(r'https://github.com/(.*).git@(.*)', repo_url)
+    match = re.match(r"https://github.com/(.*).git@(.*)", repo_url)
 
     if match:
         repo_name = match.group(1)
         branch_name = match.group(2)
     else:
-        repo_name = repo_url.rstrip('.git')
-        branch_name = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).strip().decode()
+        repo_name = repo_url.removesuffix(".git")
+        branch_name = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).strip().decode()
 
     # Commit hash
-    commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip().decode()
+    commit_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode()
 
     return repo_name, branch_name, commit_hash
 
-def mm_notification():
 
+def mm_notification():
     # Get repository information
     repo_name, branch_name, commit_hash = get_repo_info()
 
-    #Get Server Name
+    # Get Server Name
     server_name = socket.gethostname()
 
     # TODO: decide on the best path for the hook URL
-    file_path = os.path.expanduser('~/mm_webhook_url.txt')
-    with open(file_path, 'r') as file:
+    file_path = os.path.expanduser("~/mm_webhook_url.txt")
+    with open(file_path, "r") as file:
         mm_webhook_url = file.read().strip()
         if not mm_webhook_url:
             return
 
     mm_message = {
-        "text": f"⚙️**Install Information.** **Server Name:** {server_name}. **Package:** \"{repo_name[:7]}\u200B{repo_name[7:]}\". **Branch:** {branch_name}. **Commit:** {commit_hash}."
+        "text": f'⚙️**Install Information.** **Server Name:** {server_name}. **Package:** "{repo_name[:7]}\u200B{repo_name[7:]}". **Branch:** {branch_name}. **Commit:** {commit_hash}.'
     }
-    headers = {'Content-Type': 'application/json'}
+    headers = {"Content-Type": "application/json"}
     try:
         response = requests.post(mm_webhook_url, data=json.dumps(mm_message), headers=headers)
-        if response.status_code == 200:
-            print("Message sent successfully to Mattermost")
-        else:
-            print(f"Failed to send message: {response.status_code}, {response.text}")
     except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+        pass
 
 
 class CustomBuildHook(BuildHookInterface):
@@ -161,4 +156,7 @@ class CustomBuildHook(BuildHookInterface):
                 pass
 
         # update the mattermost chat-ops channel
-        mm_notification()
+        try:
+            mm_notification()
+        except:
+            pass
