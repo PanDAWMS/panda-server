c5a8904331717e468bada4a672e33fe1f01790ba
tmaeno <tmaeno@bnl.gov>
2024-12-02 13:50:36 +0100

    task_api

--- pandaserver/api/v1/task_api_tests.py
+++ pandaserver/api/v1/task_api_tests.py
@@ -1,16 +1,64 @@
-import json
+# Description: Unit tests for the Harvester API methods in the task module
+import os
 import unittest
-from datetime import datetime, timezone
 
 from pandaserver.api.v1.http_client import HttpClient, api_url, api_url_ssl
 
+# to run the tests with a valid JEDI Task ID by setting the environment variable
+JEDI_TASK_ID = os.environ.get("TEST_TASK_API_JEDI_TASK_ID", -1)
 
-class TestHarvesterAPI(unittest.TestCase):
+
+class TestTaskAPI(unittest.TestCase):
     def setUp(self):
         # Set up a mock TaskBuffer and initialize it
         self.http_client = HttpClient()
 
     # Add the unit tests for each API method here
+
+    def test_retry(self):
+        url = f"{api_url_ssl}/task/retry"
+        print(f"Testing URL: {url}")
+        data = {"jedi_task_id": JEDI_TASK_ID}
+        status, output = self.http_client.post(url, data)
+
+        actual_response = [status, output[0]]
+        if JEDI_TASK_ID == -1:
+            # Fake task should not be found
+            expected_response = [0, 2]
+        else:
+            # Real task
+            expected_response = [0, 0]
+        self.assertEqual(actual_response, expected_response)
+
+    def test_enable_job_cloning(self):
+        url = f"{api_url_ssl}/task/enable_job_cloning"
+        print(f"Testing URL: {url}")
+        data = {"jedi_task_id": JEDI_TASK_ID}
+        status, output = self.http_client.post(url, data)
+
+        actual_response = [status, output[0]]
+        if JEDI_TASK_ID == -1:
+            # Fake task should not be found
+            expected_response = [0, False]
+        else:
+            # Real task
+            expected_response = [0, True]
+        self.assertEqual(actual_response, expected_response)
+
+    def test_disable_job_cloning(self):
+        url = f"{api_url_ssl}/task/disable_job_cloning"
+        print(f"Testing URL: {url}")
+        data = {"jedi_task_id": JEDI_TASK_ID}
+        status, output = self.http_client.post(url, data)
+
+        actual_response = [status, output[0]]
+        if JEDI_TASK_ID == -1:
+            # Fake task should not be found
+            expected_response = [0, False]
+        else:
+            # Real task
+            expected_response = [0, True]
+        self.assertEqual(actual_response, expected_response)
 
 
 # Run tests
