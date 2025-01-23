4454256240c889e5906c54741ab1437fd92e4b71
Fernando Barreiro <fh.barreiro@gmail.com>
2025-01-23 15:57:16 +0100

    Making sure the casted value is passed to decorated function

--- pandaserver/api/v1/common.py
+++ pandaserver/api/v1/common.py
@@ -179,7 +179,8 @@ def request_validation(logger, secure=False, production=False, request_method=No
                 if request_method == "GET":
                     try:
                         tmp_logger.debug(f"Casting '{param_name}' to type {expected_type.__name__}.")
-                        bound_args.arguments[param_name] = expected_type(param_value)
+                        param_value = expected_type(param_value)
+                        bound_args.arguments[param_name] = param_value  # Ensure the cast value is used
                     except (ValueError, TypeError):
                         message = f"Type error: '{param_name}' could not be casted to type {expected_type.__name__}."
                         tmp_logger.error(message)
