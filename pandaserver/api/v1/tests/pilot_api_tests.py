23407ebb4c8cacf8a80d39f3e1573242e997900e
Fernando Barreiro <fh.barreiro@gmail.com>
2025-03-28 16:38:50 +0100

    Correction in pilot_api_tests.py

--- pandaserver/api/v1/tests/pilot_api_tests.py
+++ pandaserver/api/v1/tests/pilot_api_tests.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 from pandaserver.api.v1.http_client import HttpClient, api_url_ssl
 
 
-class TestTaskAPI(unittest.TestCase):
+class TestPilotAPI(unittest.TestCase):
     def setUp(self):
         self.http_client = HttpClient()
 
