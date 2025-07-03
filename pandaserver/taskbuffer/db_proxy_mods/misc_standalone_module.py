4653456fca2c9f439207a93787be910d0ef83de2
Fernando Barreiro <fh.barreiro@gmail.com>
2025-07-03 12:45:15 +0200

    Type conversion of the field inside the json

--- pandaserver/taskbuffer/db_proxy_mods/misc_standalone_module.py
+++ pandaserver/taskbuffer/db_proxy_mods/misc_standalone_module.py
@@ -225,7 +225,7 @@ class MiscStandaloneModule(BaseModule):
 
         # Get the site information from schedconfig
         sql = (
-            "SELECT /* use_json_type */ sc.data.maxtime, sc.data.corepower, sc.panda_queue, NVL(TO_NUMBER(sc.data.corecount), 1) AS corecount "
+            "SELECT /* use_json_type */ sc.data.maxtime, sc.data.corepower, NVL(TO_NUMBER(sc.data.corecount), 1) AS corecount "
             "FROM ATLAS_PANDA.schedconfig_json sc "
             "WHERE sc.panda_queue= :site_id "
         )
