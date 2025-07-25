23b85754532320c927b59b713b5b285fa3a41a53
Fernando Barreiro <fh.barreiro@gmail.com>
2025-07-25 15:29:37 +0200

    Additional job serialization in job/get_description(_incl_archive)

--- pandaserver/api/v1/job_api.py
+++ pandaserver/api/v1/job_api.py
@@ -111,7 +111,7 @@ def get_description(self, job_ids: List[int]) -> Dict:
         job_ids = []
 
     tmp_logger.debug(f"Retrieving data for {job_ids}")
-    ret = global_task_buffer.peekJobs(job_ids, use_json=True)
+    ret = JobUtils.dump_jobs_json(global_task_buffer.peekJobs(job_ids, use_json=True))
     _logger.debug("Done")
 
     return generate_response(True, data=ret)
@@ -152,7 +152,8 @@ def get_description_incl_archive(req: PandaRequest, job_ids: List[int]) -> Dict:
 
     tmp_logger.debug(f"Retrieving data for {str(job_ids)}")
 
-    ret = global_task_buffer.getFullJobStatus(job_ids)
+    ret = JobUtils.dump_jobs_json(global_task_buffer.getFullJobStatus(job_ids))
+
     tmp_logger.debug("getFullJobStatus end")
     return generate_response(True, data=ret)
 
