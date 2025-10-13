f1345c67deb448bf30434e88673eaa3a15fd998e
Fernando Barreiro <fh.barreiro@gmail.com>
2025-10-13 11:23:55 +0200

    Loose site matching when comparing sites

--- pandaserver/taskbuffer/db_proxy_mods/task_utils_module.py
+++ pandaserver/taskbuffer/db_proxy_mods/task_utils_module.py
@@ -702,7 +702,7 @@ class TaskUtilsModule(BaseModule):
 
                     # --- core power by host (CPU model) ---
                     benchmarks = []
-                    atlas_site = "NO_SITE"
+                    atlas_site = "NO_SITE"  # in case of no match
                     if site_mapper:
                         atlas_site = site_mapper.getSite(computingSite).pandasite
                         benchmarks = self.get_cpu_benchmarks_by_host(atlas_site, modificationhost) or []
