43726e6d2d1cb35bad8c0495a9964840cd804c29
Fernando Barreiro <fh.barreiro@gmail.com>
2025-10-23 16:40:12 +0200

    killTaskJEDI.py corrections

--- pandaserver/test/killTaskJEDI.py
+++ pandaserver/test/killTaskJEDI.py
@@ -2,12 +2,10 @@ import optparse
 
 import pandaserver.userinterface.Client as Client
 
-aSrvID = None
-
 optP = optparse.OptionParser(conflict_handler="resolve")
 options, args = optP.parse_args()
 
-task_id = args[0]
+task_id = int(args[0])
 
 s, o = Client.kill_task(task_id)
 print(o)
