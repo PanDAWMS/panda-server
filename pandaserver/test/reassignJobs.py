e53f3b68b541eebd941b8c142901a7263453802e
Fernando Barreiro <fh.barreiro@gmail.com>
2025-10-23 16:43:27 +0200

    reassignJobs.py corrections

--- pandaserver/test/reassignJobs.py
+++ pandaserver/test/reassignJobs.py
@@ -4,7 +4,7 @@ import pandaserver.userinterface.Client as Client
 
 if len(sys.argv) == 2:
     job_id = int(sys.argv[1])
-    Client.reassign_jobs([job_id])
+    ret = Client.reassign_jobs([job_id])
 else:
     job_id_start = int(sys.argv[1])
     job_id_end = int(sys.argv[2])
@@ -12,4 +12,6 @@ else:
         print(f"{job_id_end} is less than {job_id_start}")
         sys.exit(1)
 
-    Client.reassign_jobs(range(job_id_start, job_id_end + 1))
+    ret = Client.reassign_jobs(range(job_id_start, job_id_end + 1))
+
+print(ret)
