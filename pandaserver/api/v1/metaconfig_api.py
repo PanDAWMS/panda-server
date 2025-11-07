510a55b9cec47a63c834daa148c3c7edc6930e71
Fernando Barreiro <fh.barreiro@gmail.com>
2025-11-07 11:53:53 +0100

    Panglia API migration: disable proxy requirement

--- pandaserver/api/v1/metaconfig_api.py
+++ pandaserver/api/v1/metaconfig_api.py
@@ -51,7 +51,7 @@ def get_banned_users(req: PandaRequest) -> dict:
     return generate_response(success, data=users)
 
 
-@request_validation(_logger, secure=True, request_method="GET")
+@request_validation(_logger, secure=False, request_method="GET")
 def get_site_specs(req: PandaRequest, type: str = "analysis") -> dict:
     """
     Get site specs
