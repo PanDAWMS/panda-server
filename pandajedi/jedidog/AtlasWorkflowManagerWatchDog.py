b29aa284c2739b4f6d4cd12e95719b72ee25577e
mightqxc <mightqxc@hotmail.com>
2025-11-27 09:21:20 +0100

    workflows4: rename watchdog to be workflow manager

--- pandajedi/jedidog/AtlasWorkflowManagerWatchDog.py
+++ pandajedi/jedidog/AtlasWorkflowManagerWatchDog.py
@@ -18,9 +18,9 @@ from .WatchDogBase import WatchDogBase
 logger = PandaLogger().getLogger(__name__.split(".")[-1])
 
 
-class AtlasWorkflowProcessorWatchDog(WatchDogBase):
+class AtlasWorkflowManagerWatchDog(WatchDogBase):
     """
-    Workflow processor watchdog for ATLAS
+    Workflow manager watchdog for ATLAS
     """
 
     # constructor
@@ -37,7 +37,7 @@ class AtlasWorkflowProcessorWatchDog(WatchDogBase):
         tmpLog.debug("start")
         try:
             # watchdog lock
-            got_lock = self.get_process_lock("AtlasWFProcDog.doProcessWorkflows", timeLimit=1)
+            got_lock = self.get_process_lock("AtlasWFManagerDog.doProcessWorkflows", timeLimit=1)
             if not got_lock:
                 tmpLog.debug("locked by another watchdog process. Skipped")
                 return
