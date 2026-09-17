f4d4262a0c54fa65065cd75105d5e422b3c88011
tmaeno <tmaeno@bnl.gov>
2026-09-17 09:42:17 +0200

    msg

--- pandajedi/jedirefine/TaskRefinerBase.py
+++ pandajedi/jedirefine/TaskRefinerBase.py
@@ -647,12 +647,14 @@ class TaskRefinerBase(object):
                             if self.taskSpec.is_work_segmented():
                                 tmpDatasetNameList *= len(taskParamMap["segmentSpecs"])
                         else:
+                            self.tmpLog.debug("1")
                             tmpIF = self.ddmIF.getInterface(self.taskSpec.vo, self.taskSpec.cloud)
                             if not tmpIF:
                                 tmpDatasetNameList = []
                             else:
                                 # get datasets in dataset container
                                 dataset_names_in_container = tmpIF.expandContainer(datasetName)
+                                self.tmpLog.debug("2")
                                 # sort datasets to process online complete replicas first
                                 tmp_ok_list: list[Any] = []
                                 tmp_ng_list = []
@@ -661,9 +663,11 @@ class TaskRefinerBase(object):
                                     if len(tmp_ok_list) > 10:
                                         is_ok = True
                                     else:
+                                        self.tmpLog.debug("3")
                                         # check if complete replica is available at online endpoint
                                         is_ok = False
                                         tmp_dict = tmpIF.listDatasetReplicas(tmp_dataset_name_in_container)
+                                        self.tmpLog.debug("4")
                                         for tmp_endpoint, tmp_data_list in tmp_dict.items():
                                             tmp_data = tmp_data_list[0]
                                             if (
@@ -683,8 +687,10 @@ class TaskRefinerBase(object):
                                     tmpDatasetNameList = dataset_names_in_container
                                 else:
                                     # normal dataset name
+                                    self.tmpLog.debug("5")
                                     tmpDatasetNameList = tmpIF.listDatasets(datasetName)
                                     constituent_dataset_names_not_used_as_input = [i for i in dataset_names_in_container if i not in tmpDatasetNameList]
+                                    self.tmpLog.debug("6")
                         self.tmpLog.debug("loop over all element names")                    
                         i_element = 0
                         for elementDatasetName in tmpDatasetNameList:
